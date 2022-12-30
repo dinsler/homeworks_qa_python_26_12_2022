47d87cfbafc0e77851a9928a2fad09f6b4b53ace
dinsler <hanna.dinsler@gmail.com>
2022-12-31 00:46:29 +0200

    Init commit

---
+++ homework1.py
@@ -0,0 +1,44 @@
+# Task 1. create two variables first=10, second=30, then display the result of mathematical interaction (+, -, *, /, etc.) for these numbers
+first = 10
+second = 30
+
+add = first + second
+print(add)
+
+subtract = first - second
+print(subtract)
+
+multiply = first * second
+print(multiply)
+
+divide = first / second
+print(divide)
+
+floor_divide = first // second
+print(floor_divide)
+
+exponent = first ** second
+print(exponent)
+
+modulo = first % second
+print(modulo)
+
+# Task 2.Create a variable and one by one write the result of the comparison (<, > , ==, !=) of the numbers from task 1 into it. Display the result of each comparison on the screen.
+my_bool = True
+my_bool = first < second
+print(my_bool)
+
+my_bool = first > second
+print(my_bool)
+
+my_bool = first <= second
+print(my_bool)
+
+my_bool = first >= second
+print(my_bool)
+
+my_bool = first == second
+print(my_bool)
+
+my_bool = first != second
+print(my_bool)
