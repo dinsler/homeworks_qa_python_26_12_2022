c805dfd95e808ecd3ff8764d05beff061baa544d
dinsler <hanna.dinsler@gmail.com>
2023-01-25 13:00:14 +0200

    homework_lesson6_function

--- lesson6.py
+++ lesson6.py
@@ -1,5 +1,7 @@
 # Task2. Write a movie theater cashier program that asks the user to enter their age.
 # Do it all with a function
+from typing import Union
+
 
 def cashier_job():
     while True:
@@ -14,14 +16,13 @@ def cashier_job():
 
         print(message)
 
-        if_continue = input("Чи хочете ви знову ввести свій вік?: ")
-        if if_continue == "так":
-            continue
-        else:
+        if_continue = input("Чи хочете ви знову ввести свій вік? Введіть так чи ні : ")
+        if if_continue != "так":
+            print("Заходьте ще!")
             break
 
 
-def check_input(user_message):
+def check_input(user_message: str) -> Union[int, None]:
     """
      check if user_message has only digits
     :return: user_message as integer
@@ -29,25 +30,30 @@ def check_input(user_message):
 
     try:
         res = int(user_message)
+        if res not in range(1, 100):
+            print("Please enter valid age!")
+            return None
         return res
     except ValueError:
         print("enter number, please")
 
 
-def find_ending(user_age):
+def find_ending(user_age: int) -> str:
     """
     depending on age we will return appropriate ending
     :return: ending as string
     """
-    if (user_age % 10 == 1) and (user_age != 11) and (user_age != 111):
-        return "рік"
-    elif (user_age % 10 > 1) and (user_age % 10 < 5) and (user_age != 12) and (user_age != 13) and (user_age != 14):
-        return "роки"
-    else:
-        return "років"
+    res = "років"
+
+    if user_age % 10 == 1 and user_age != 11:
+        res = "рік"
+    elif 1 < user_age % 10 < 5 and user_age not in [12, 13, 14]:
+        res = "роки"
+
+    return res
 
 
-def get_message(user_age, ending):
+def get_message(user_age: int, ending: str) -> str:
     """
     depending on user age provide appropriate cashier message
     :return: message as a string
@@ -55,17 +61,19 @@ def get_message(user_age, ending):
     user_age_as_string = str(user_age)
     symbol = user_age_as_string[0]
     count = user_age_as_string.count(symbol)
+
+    res = f"Незважаючи на те, що вам {user_age} {ending}, білетів всеодно нема!"
+
     if count > 1:
-        return f"О, вам {user_age} {ending}! Який цікавий вік!"
-
-    if user_age < 7:
-        return f"Тобі ж {user_age} {ending}! Де твої батьки?"
+        res = f"О, вам {user_age} {ending}! Який цікавий вік!"
+    elif user_age < 7:
+        res = f"Тобі ж {user_age} {ending}! Де твої батьки?"
     elif user_age < 16:
-        return f"Тобі лише {user_age} {ending}, а це е фільм для дорослих!"
+        res = f"Тобі лише {user_age} {ending}, а це е фільм для дорослих!"
     elif user_age > 65:
-        return f"Вам {user_age} {ending}? Покажіть пенсійне посвідчення?"
-    else:
-        return f"Незважаючи на те, що вам {user_age} {ending}, білетів всеодно нема!"
+        res = f"Вам {user_age} {ending}? Покажіть пенсійне посвідчення?"
+
+    return res
 
 
-print(cashier_job())
+cashier_job()
